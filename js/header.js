51e2b065d21fbdf3d5d2a591d01012f2dc6a9e71
mahpgnaohhnim <minhhoangpham@live.de>
2017-05-30 16:10:34 +0200

    commiting files

---
+++ js/header.js
@@ -0,0 +1,6 @@
+//var header = function(){
+  let header = document.getElementsByTagName('header')[0];
+  const logo = "<div id='logo'></div>"
+  header.innerHTML="<h2>HEADER</h2>";
+
+//}
---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+  loadConf();
+});
+
+function loadJS(filename){
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.type = "text/javascript";
+  script.src = "js/"+filename+".js";
+  head.appendChild(script);
+}
+
+function loadConf(){
+  loadJS('header');
+}
