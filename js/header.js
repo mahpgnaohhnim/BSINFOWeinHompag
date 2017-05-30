55b3c87df1a1d781444cf3841ae3f58ea97296aa
mahpgnaohhnim <minhhoangpham@live.de>
2017-05-30 18:21:30 +0200

    now loading JS successful

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 class Header{
   constructor(){
-    this = document.getElementsByTagName('header')[0];
+    var pub = document.getElementsByTagName('header')[0];
     const logo = "<div id='logo'>Test</div>"
-    this.innerHTML = logo;
+    pub.innerHTML = logo;
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
   loadConf();
-  init();
 });
 
 function loadJS(filename){
