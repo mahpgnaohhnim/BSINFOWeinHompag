a29eb3b5444de06f7f37d7dc0b4f870af68365ed
mahpgnaohhnim <minhhoangpham@live.de>
2017-05-30 18:19:26 +0200

    test

--- js/header.js
+++ js/header.js
@@ -2,6 +2,6 @@ class Header{
   constructor(){
     this = document.getElementsByTagName('header')[0];
     const logo = "<div id='logo'>Test</div>"
-    header.innerHTML = logo;
+    this.innerHTML = logo;
   }
 }
